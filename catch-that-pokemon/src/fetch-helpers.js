f4b65f4b8198e7b4c7cba5fd77adc6af1b1be7f9
Jandir <jandirgregorio@icloud.com>
2026-02-11 20:20:57 -0500

    implemented fetch-helpers, dom-helpers, and main

--- catch-that-pokemon/src/fetch-helpers.js
+++ catch-that-pokemon/src/fetch-helpers.js
@@ -4,15 +4,39 @@ export const getRandomPokemon = async () => {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
     if (!response.ok) throw Error(`Fetch failed. ${response.status} ${response.statusText}`);
     const data = await response.json();
+    
+    const types = data.types.map((pokeType) => pokeType.type.name).join(', ');
     const pokemonObj = {
       name: data.name,
-      types: data.types.map((type) => type.type.name).join(', '),
-      sprite: data.sprites.back_default,
+      types: types,
+      sprite: data.sprites.front_default,
     }
     return { data: pokemonObj, error: null};
   }
   catch (error) {
-    console.log(error);
+    console.log(error)
+    return { data: null, error};
+  }
+};
+
+export const postDiscoveredPokemon = async (formData) => {
+  try {
+    const config = {
+      method: 'POST',
+      body: JSON.stringify(formData),
+      headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json'
+      }
+    };
+
+    const response = await fetch('https://formspree.io/f/maqdqzlk', config);
+    if (!response.ok) throw Error(`Fetch failed. ${response.status} ${response.statusText}`);
+
+    const responseData = await response.json();
+    return { data: responseData, error: null };
+  }
+  catch (error) {
     return { data: null, error};
   }
 };
--- catch-that-pokemon/src/main.js
+++ catch-that-pokemon/src/main.js
@@ -1,3 +1,43 @@
-import { getRandomPokemon } from "./fetch-helpers";
-import { renderPokemon } from "./dom-helpers";
+import { getRandomPokemon, postDiscoveredPokemon } from "./fetch-helpers";
+import { renderPokemon, renderError, renderSuccess} from "./dom-helpers";
 
+const pokemonList = document.querySelector('#discover-button');
+const pokemonForm = document.querySelector('#pokemon-form');
+
+const getAndRenderPokemon = async () => {
+  const pokemon = await getRandomPokemon();
+  const { data, error } = pokemon;
+  if (data === null) {
+    renderSuccess('');
+    renderError(error.message);
+  } else {
+    renderError('');
+    renderPokemon(data);
+    renderSuccess(`${data.name} was discovered!`);
+  }
+};
+
+pokemonList.addEventListener('click', () => {
+  getAndRenderPokemon();
+});
+
+pokemonForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const formData = new FormData(pokemonForm);
+  const formValues = Object.fromEntries(formData);
+  const { name, types, isFavorite } = formValues;
+  formValues.isFavorite = Boolean(isFavorite);
+
+  const pokemon = await postDiscoveredPokemon(formValues);
+
+  if (pokemon.data === null) {
+    renderSuccess('');
+    renderError('Error: unable to capture Pokémon. Please try again later.');
+  } else {
+    renderError('');
+    renderSuccess(`${name} (${types}) has been captured!`);
+    pokemonForm.reset()
+  }
+});
+
+getAndRenderPokemon();
